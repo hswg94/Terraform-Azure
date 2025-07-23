993034731bb69a01db390c0f8c85876acee4b191
hswg94 <hswg94@gmail.com>
2025-07-23 15:59:49 +0800

    Refactor Azure networking configuration: update virtual network and subnet settings, disable default outbound access, and add service endpoints for storage

--- main.tf
+++ main.tf
@@ -4,34 +4,82 @@ resource "azurerm_resource_group" "testproject-rg" {
   location = "Southeast Asia"
 }
 
-# Create a virtual network within the resource group
+# virtual network
 resource "azurerm_virtual_network" "testproject-vnet" {
-  name                = "testproject-vnet"
+  name                = "vnet-uat-ppl"
   resource_group_name = azurerm_resource_group.testproject-rg.name
   location            = azurerm_resource_group.testproject-rg.location
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["172.18.132.0/24"]
+  dns_servers = ["172.18.17.68", "172.18.17.69"]
 }
 
+# Application Gateway subnet
 resource "azurerm_subnet" "testproject-agw-subnet" {
-  name                 = "testproject-agw"
+  name                 = "sub-ppl-uatweb-appgw01"
   resource_group_name  = azurerm_resource_group.testproject-rg.name
   virtual_network_name = azurerm_virtual_network.testproject-vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["172.18.132.0/28"]
+
+  # Disable default outbound access
+  default_outbound_access_enabled = false
 }
 
-resource "azurerm_subnet" "testproject-vmss-subnet" {
-  name                 = "testproject-vmss"
+# Application subnet for VMs
+resource "azurerm_subnet" "testproject-app-subnet" {
+  name                 = "sub-ppl-uatapt-app01"
   resource_group_name  = azurerm_resource_group.testproject-rg.name
   virtual_network_name = azurerm_virtual_network.testproject-vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["172.18.132.16/28"]
+
+  # Disable default outbound access
+  default_outbound_access_enabled = false
+
+  # Service endpoints for storage
+  service_endpoints = ["Microsoft.Storage"]
 }
 
+# Database subnet for private endpoints
+resource "azurerm_subnet" "testproject-db-subnet" {
+  name                 = "sub-ppl-uatdbt-dbs01"
+  resource_group_name  = azurerm_resource_group.testproject-rg.name
+  virtual_network_name = azurerm_virtual_network.testproject-vnet.name
+  address_prefixes     = ["172.18.132.48/28"]
+
+  # Disable default outbound access
+  default_outbound_access_enabled = false
+
+  # Service endpoints for storage
+  service_endpoints = ["Microsoft.Storage"]
+}
+
+# Management/Jump Host subnet
+resource "azurerm_subnet" "testproject-mgmt-subnet" {
+  name                 = "sub-ppl-uatmgt-jh01"
+  resource_group_name  = azurerm_resource_group.testproject-rg.name
+  virtual_network_name = azurerm_virtual_network.testproject-vnet.name
+  address_prefixes     = ["172.18.132.224/28"]
+
+  # Disable default outbound access
+  default_outbound_access_enabled = false
+}
+
+# Network Security Group Associations
 resource "azurerm_subnet_network_security_group_association" "agw-nsg-association" {
   subnet_id                 = azurerm_subnet.testproject-agw-subnet.id
   network_security_group_id = azurerm_network_security_group.testproject-agw-subnet-nsg.id
 }
 
-resource "azurerm_subnet_network_security_group_association" "vmss-nsg-association" {
-  subnet_id                 = azurerm_subnet.testproject-vmss-subnet.id
+resource "azurerm_subnet_network_security_group_association" "app-nsg-association" {
+  subnet_id                 = azurerm_subnet.testproject-app-subnet.id
   network_security_group_id = azurerm_network_security_group.testproject-vmss-subnet-nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "db-nsg-association" {
+  subnet_id                 = azurerm_subnet.testproject-db-subnet.id
+  network_security_group_id = azurerm_network_security_group.testproject-db-subnet-nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "mgmt-nsg-association" {
+  subnet_id                 = azurerm_subnet.testproject-mgmt-subnet.id
+  network_security_group_id = azurerm_network_security_group.testproject-mgmt-subnet-nsg.id
 }
