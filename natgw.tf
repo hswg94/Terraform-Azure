c24dac818129894be868e8879d39dd150d3e6719
hswg94 <hswg94@gmail.com>
2025-07-23 16:36:12 +0800

    Add NAT Gateway and Public IP resources for improved network configuration

---
+++ natgw.tf
@@ -0,0 +1,23 @@
+# Public IP for NAT Gateway
+resource "azurerm_public_ip" "pip-ppl-uat-natgw01" {
+  name                = "pip-ppl-uat-natgw01"
+  location            = azurerm_resource_group.testproject-rg.location
+  resource_group_name = azurerm_resource_group.testproject-rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# NAT Gateway
+resource "azurerm_nat_gateway" "natgw-uat-ppl" {
+  name                    = "natgw-uat-ppl"
+  location                = azurerm_resource_group.testproject-rg.location
+  resource_group_name     = azurerm_resource_group.testproject-rg.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 4
+}
+
+# Associate Public IP with NAT Gateway
+resource "azurerm_nat_gateway_public_ip_association" "natgw-pip-association" {
+  nat_gateway_id       = azurerm_nat_gateway.natgw-uat-ppl.id
+  public_ip_address_id = azurerm_public_ip.pip-ppl-uat-natgw01.id
+}
